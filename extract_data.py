2cbda07b57748c6044d50c82e62f8b5cfd57f0c5
Frank Hui <huifrank@live.com>
2018-12-29 16:35:22 -0800

    Rename main.py to visualise_data.py and make extract_data.py more user friendly

--- extract_data.py
+++ extract_data.py
@@ -1,52 +1,47 @@
 import pydicom as dicom
 import os
 import numpy
-from matplotlib import pyplot
 import sys
+import time
 
-path_data = "../Aura_Data/RIDER NEURO MRI"
+sys.stderr.write("WARNING: All .dcm files must have the same image dimensions.\n")
+time.sleep(0.01)
+path_data = input("Path to folder containing all .dcm files: ")
+newFilename = input("Filename to dump information into: ")
+if ".aura" not in newFilename:
+    newFilename += ".aura"
+print("Saving to " + newFilename)
+time.sleep(5)
+
 lstFilesDCM = []
-i = 0
-progress_bar = "[                    ] 0%"
+print("Reading path...")
 for dirName, subdirList, fileList in os.walk(path_data) :
     for filename in fileList:
         if ".dcm" in filename.lower():
-            sys.stdout.write('\r')
-            bar = ""
-            for x in range((int)((i/20060) * 20)):
-                bar += "="
-            for x in range((int)((1 - (i/20060)) * 20)):
-                bar += " "
-            progress_bar = "[" + bar + "]" + str((int)((i/20060) * 100)) + "%"
-            sys.stdout.write(progress_bar)
-            sys.stdout.flush()
-
+            print("Loaded " + filename)
             lstFilesDCM.append(os.path.join(dirName,filename))
-            i += 1
 
-sys.stdout.write('\r')
-progress_bar = "[====================] 100%"
-sys.stdout.write(progress_bar)
-sys.stdout.flush()
-print("\n" + str(lstFilesDCM.__sizeof__()) + " filenames read.")
+print("\n" + str(lstFilesDCM.__sizeof__()) + " file names read.")
 lstFilesDCM.sort()
 
 RefDs = dicom.read_file(lstFilesDCM[0])
 
 ConstPixelDims = (int(RefDs.Rows), int(RefDs.Columns), len(lstFilesDCM))
-
 ConstPixelSpacing = (float(RefDs.PixelSpacing[0]), float(RefDs.PixelSpacing[1]), float(RefDs.SliceThickness))
 
 x = numpy.arange(0.0, (ConstPixelDims[0]+1)*ConstPixelSpacing[0], ConstPixelSpacing[0])
 y = numpy.arange(0.0, (ConstPixelDims[1]+1)*ConstPixelSpacing[1], ConstPixelSpacing[1])
 z = numpy.arange(0.0, (ConstPixelDims[2]+1)*ConstPixelSpacing[2], ConstPixelSpacing[2])
 
-ArrayDicom = numpy.zeros((256,256,len(lstFilesDCM)), dtype=RefDs.pixel_array.dtype)
+ArrayDicom = numpy.zeros(ConstPixelDims, dtype=RefDs.pixel_array.dtype)
 
 # loop through all the DICOM files
+print("Loading images into numpy array...")
 badFiles = 0
 goodFiles = 0
-i = 0
+image_num = 0
+progress_bar_length = 50;
+
 for filenameDCM in lstFilesDCM:
     # print("Extracting " + filenameDCM)
     # read the file
@@ -58,20 +53,21 @@ for filenameDCM in lstFilesDCM:
         goodFiles += 1
     except:
         badFiles += 1
-        # sys.stderr.write("Exception occurred for file " + filenameDCM + "\n")
+        # sys.stderr.write("Exception occurred for file " + filenameDCM + " dimensions invalid; should match dimensions of first image.\n")
     sys.stdout.write('\r')
     bar = ""
-    for x in range((int)((i / len(lstFilesDCM)) * 50)):
-        bar += "="
-    for x in range((int)((1 - (i / len(lstFilesDCM))) * 50)):
+    for x in range((int)((image_num / len(lstFilesDCM)) * progress_bar_length)):
+        bar += "▮"
+    for x in range((int)((1 - (image_num / len(lstFilesDCM))) * progress_bar_length)):
         bar += " "
-    progress_bar = "[" + bar + "]" + str((int)((i / len(lstFilesDCM)) * 100)) + "%"
+    progress_bar = "[" + bar + "]" + str((int)((image_num / len(lstFilesDCM)) * 100)) + "%"
     sys.stdout.write(progress_bar)
     sys.stdout.flush()
-    i += 1
+    image_num += 1
 
-ArrayDicom.tofile("RIDER_Data")
-print("DATA EXTRACTION COMPLETE.")
-print(badFiles, " bad files found and not read.")
-print(goodFiles, " good files found and read.")
+ArrayDicom.tofile(newFilename)
+print("\n\n----------------------- DATA EXTRACTION COMPLETE. -----------------------")
+print(badFiles, " bad files found and not read because of invalid dimensions.")
+print(goodFiles, " good files found and read, with proper dimensions.")
+print("Your matrix dimensions are (length, width, number of images): ", ConstPixelDims)
 
