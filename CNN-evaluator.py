6352fa1f1aa55e5be34a6e6798b0a796220150a4
Frank Hui <huifrank@live.com>
2019-01-07 21:55:27 -0800

    Adjust model

--- CNN-evaluator.py
+++ CNN-evaluator.py
@@ -8,9 +8,10 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 import numpy
+from aura.extractor_util import reshape
 from matplotlib import pyplot as plt
-
-from aura import aura_loader as al
+from aura.extractor_util import parseAuraDimensions as pAD
+from aura.aura_loader import read_file
 from aura import extractor_util as eu
 
 model = Sequential()
@@ -19,19 +20,47 @@ model.add(Conv2D(32, kernel_size=(3, 3),
                  input_shape=(256,256,1)))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(2, activation='softmax'))
 
-model.load_weights("Model-v1.hf")
+model.load_weights("Model-v3.hf")
 
-images = al.read_file("../Aura_Data/ChunkedHealthyTestset/{136x136x181}Chunk0.aura").T
-image = np.zeros((1,256,256,1))
-image[0] = eu.convertToSize(images[79],(256,256)).reshape(256,256,1)
+root = "../Aura_Data/";
+cancerPath = root + "ChunkedCancerTestset/"
+healthyPath = root + "ChunkedHealthyTestset/"
+cancerSize = "{256x256x270}"
+healthySize = "{136x136x181}"
 
-plt.imshow(images[79])
-plt.show()
+cl,cw,cn = pAD(cancerSize)
+hl,hw,hn = pAD(healthySize)
+fl, fw = max(cl, cw, hl, hw), max(cl, cw, hl, hw)
+fn = cn + hn
 
-print(model.predict(image))
+cancerous_test_data = read_file(path=cancerPath + cancerSize + "Chunk1.aura").T
+healthy_test_data = read_file(path=healthyPath + healthySize + "Chunk1.aura")
+healthy_test_data = reshape(healthy_test_data, (fl,fw, hn)).T
+test_data = np.zeros((fn, fl,fw))
+for i in range(cn):
+    test_data[i] = cancerous_test_data[i]
+for i in range(hn):
+    test_data[i + cn] = healthy_test_data[i]
+
+labels = np.zeros(fn)
+for i in range(cn):
+    labels[i] = 1
+
+x_test = test_data
+y_test = labels
+
+x_test = test_data.reshape(fn,fl,fw,1)
+
+model.compile(loss=keras.losses.sparse_categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
--- CNN.py
+++ CNN.py
@@ -7,7 +7,7 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-
+import random
 
 from aura.extractor_util import reshape
 from aura.extractor_util import parseAuraDimensions as pAD
@@ -16,19 +16,19 @@ from aura.aura_loader import read_file
 print("Modules imported.")
 print(os.getcwd())
 root = "../Aura_Data/";
-cancerPath = root + "ChunkedRIDER/"
-healthyPath = root + "ChunkedHealthy/"
+cancerPath = root + "Dataset/"
+healthyPath = root + "Dataset/"
 
-cancerSize = "{256x256x3511}"
-healthySize = "{136x136x2353}"
+cancerSize = "{256x256x270}"
+healthySize = "{136x136x181}"
 
 cl,cw,cn = pAD(cancerSize)
 hl,hw,hn = pAD(healthySize)
 fl, fw = max(cl, cw, hl, hw), max(cl, cw, hl, hw)
 fn = cn + hn
 # Set up data
-cancerous_train_data = read_file(path=cancerPath + cancerSize + "Chunk0.aura").T
-healthy_train_data = read_file(path=healthyPath+ healthySize + "Chunk0.aura")
+cancerous_train_data = read_file(path=cancerPath + cancerSize + "CancerTrainset.aura").T
+healthy_train_data = read_file(path=healthyPath+ healthySize + "HealthyTrainset.aura")
 healthy_train_data = reshape(healthy_train_data, (fl,fw,hn)).T
 train_data = np.zeros((fn, fl,fw))
 for i in range(cn):
@@ -37,8 +37,21 @@ for i in range(hn):
     train_data[i + cn] = healthy_train_data[i]
 print(train_data.shape)
 
-cancerous_test_data = read_file(path=cancerPath + cancerSize + "Chunk1.aura").T
-healthy_test_data = read_file(path=healthyPath + healthySize + "Chunk1.aura")
+training = []
+for i in range(cn):
+    training.append([train_data[i], 1])
+for i in range(hn):
+    training.append([train_data[i + cn], 0])
+random.shuffle(training)
+
+train_label = np.zeros(fn)
+train_data = np.zeros(train_data.shape)
+for i,(data,label) in enumerate(training):
+    train_data[i] = data
+    train_label[i] = label
+
+cancerous_test_data = read_file(path=cancerPath + cancerSize + "CancerTestset.aura").T
+healthy_test_data = read_file(path=healthyPath + healthySize + "HealthyTestset.aura")
 healthy_test_data = reshape(healthy_test_data, (fl,fw, hn)).T
 test_data = np.zeros((fn, fl,fw))
 for i in range(cn):
@@ -47,9 +60,18 @@ for i in range(hn):
     test_data[i + cn] = healthy_test_data[i]
 print(test_data.shape)
 
-labels = np.zeros(fn)
+testing = []
 for i in range(cn):
-    labels[i] = 1
+    testing.append([test_data[i], 1])
+for i in range(hn):
+    testing.append([test_data[i + cn], 0])
+random.shuffle(testing)
+
+test_label = np.zeros(fn)
+test_data = np.zeros(test_data.shape)
+for i,(data,label) in enumerate(testing):
+    test_data[i] = data
+    test_label[i] = label
 
 batch_size = 8
 num_classes = 2
@@ -58,8 +80,8 @@ epochs = 8
 # input image dimensions
 img_rows, img_cols = fl,fw
 
-y_train = labels.copy()
-y_test = labels.copy()
+y_train = train_label.copy()
+y_test = test_label.copy()
 
 x_train = train_data.reshape(fn,fl,fw,1)
 x_test = test_data.reshape(fn,fl,fw,1)
@@ -79,10 +101,19 @@ model.add(Conv2D(32, kernel_size=(3, 3),
                  input_shape=(fl,fw,1)))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.45))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(128, activation='relu'))
+
 model.add(Dense(num_classes, activation='softmax'))
 
 model.compile(loss=keras.losses.categorical_crossentropy,
