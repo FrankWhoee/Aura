85460df07e19c42871d72db140c3300387f111f2
FrankWhoee <huifrank@live.com>
2019-02-08 09:44:26 -0800

    Fix paths

--- CNN-single-demo.py
+++ CNN-single-demo.py
@@ -9,8 +9,8 @@ root = "../Aura_Data/";
 
 model = load_model("Model-10-1.hdf5")
 # image = read_file(root + "ChunkedHealthyTestset/{136x136x181}Chunk1.aura").T[50]
-imageCancer = read_file("CancerDataset/{256x256x7021}RIDERTestset.aura").T[5021]
-imageHealthy = read_file("HealthyDataset/{136x136x22118}HealthyTestset.aura").T[5021]
+imageCancer = read_file(root + "{256x256x7021}RIDERTestset.aura").T[5021]
+imageHealthy = read_file(root + "{136x136x22118}HealthyTestset.aura").T[5021]
 # image = dcm.read_file(root + "Unextracted/CPTAC-GBM/C3L-00016/11-15-1999-MR BRAIN WOW CONTRAST-47088/8-AX 3D SPGR-43615/000199.dcm").pixel_array
 
 
