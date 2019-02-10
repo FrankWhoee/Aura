3a234260f5dd89412c2203e4e8a5a00206200df1
Frank Hui <huifrank@live.com>
2019-02-10 10:47:57 -0800

    Switch to newer model

--- CNN-visualiser.py
+++ CNN-visualiser.py
@@ -1,10 +1,7 @@
 from keras.utils import plot_model
 from keras.models import load_model
-from IPython.display import SVG
-# from keras.utils import mo
 print("Loading model...")
-model = load_model("Model-10-2.hdf5")
+model = load_model("Model-11-1.hf")
 print("Model loaded. Plotting model...")
 plot_model(model, to_file='model.png', show_shapes=True)
 print("Plotting complete. File is ready at model.png")
-# SVG(model_to_dot(model).create(prog='dot', format='svg'))
