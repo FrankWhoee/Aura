3b7724894b121e1d929f9ee4f0b672aa5a01646c
Frank Hui <huifrank@live.com>
2019-03-29 17:26:56 -0700

    Clean up and make everything into functions

--- CNN-visualiser.py
+++ CNN-visualiser.py
@@ -1,54 +1,60 @@
-# from keras.utils import plot_model
-# from keras.models import load_model
-# print("Loading model...")
-# model = load_model("Model-11.hf")
-# print("Model loaded. Plotting model...")
-# plot_model(model, to_file='model.png', show_shapes=True)
-# print("Plotting complete. File is ready at model.png")
-
 from keras.utils import plot_model
 from keras.models import load_model
 from vis.visualization.saliency import visualize_saliency
 from vis.visualization.activation_maximization import visualize_activation
 from matplotlib import pyplot
-import numpy
-from scipy.misc import imresize
-from scipy.misc import imshow
-from aura.aura_loader import read_file
 
-print("Loading images...")
-root = "../Aura_Data/Dataset/"
-cancer_path = "../Aura_Data/Chunked/ChunkedCPTAC/{256x256x10861}Chunk0.aura"
-healthy_path = root + "{136x136x22118}HealthyTestset.aura"
-# imgh = read_file(healthy_path).T[5]
-# imageHealthy = imresize(imgh,(256,256)).reshape(1,256,256,1)
-# imshow(imgh)
-imgc = read_file(cancer_path).T[10]
-imageCancer = imgc.reshape(1,256,256,1)
-imshow(imgc)
-print("Image loaded.")
 
-print("Loading model...")
-model = load_model("Model-11.hf")
-print("Model loaded.")
-print("Visualising filters...")
-layers = visualize_saliency(model=model,layer_idx=10, filter_indices=None, seed_input=imageCancer)
-print(layers.shape)
-print("Filters visualised.")
-pyplot.figure(dpi=300)
-pyplot.axes().set_aspect('equal', 'datalim')
-pyplot.pcolormesh(layers[:,:,2])
-pyplot.show()
+def generate_model_image(model_path):
+    """
+    Generates a flow chart of the model to model.png
+    :param model_path: Path of the Keras model to be loaded. Expects string input.
+    :return: None
+    """
+    print("Loading model...")
+    model = load_model(model_path)
+    print("Model loaded. Plotting model...")
+    plot_model(model, to_file='model.png', show_shapes=True)
+    print("Plotting complete. File is ready at model.png")
 
-# print("Loading model...")
-# model = load_model("Model-11.hf")
-# print("Model loaded.")
-# print("Visualising filters...")
-# layers = visualize_activation(model,layer_idx=13)
-# print(layers.shape)
-# print("Filters visualised.")
-# pyplot.figure(dpi=300)
-# pyplot.axes().set_aspect('equal', 'datalim')
-# # pyplot.set_cmap(pyplot.gray())
-# pyplot.pcolormesh(layers[:,:,0])
-# pyplot.show()
+
+def visualize_saliency_map(model_path, layer, image, sensitivity):
+    """
+    Visualises saliency maps for the specified image.
+    :param model_path: Path of the Keras model to be loaded. Expects string input.
+    :param layer: Which layer of the neural network is to be visualised. Expects integer input.
+    :param image: Image to be analyzed by the neural network to create saliency map. Expects 2D matrix input.
+    :param sensitivity: Saliency sensitivity. Goes from 0 to 3. Expects integer input.
+    :return: None
+    """
+    print("Loading model...")
+    model = load_model(model_path)
+    print("Model loaded.")
+    print("Visualising saliency map...")
+    layers = visualize_saliency(model=model, layer_idx=layer, filter_indices=None, seed_input=image)
+    print(layers.shape)
+    print("Filters visualised.")
+    pyplot.figure(dpi=300)
+    pyplot.axes().set_aspect('equal', 'datalim')
+    pyplot.pcolormesh(layers[:, :, sensitivity])
+    pyplot.show()
+
+
+def visualize_feature_maps(model_path, layer):
+    """
+    Visualises the feature maps within a Keras model.
+    :param model_path: Path of the Keras model to be loaded. Expects string input.
+    :param layer: Which layer of the model to extract a feature map of. Expects integer input.
+    :return: None
+    """
+    print("Loading model...")
+    model = load_model(model_path)
+    print("Model loaded.")
+    print("Visualising filters...")
+    layers = visualize_activation(model, layer_idx=layer)
+    print(layers.shape)
+    print("Filters visualised.")
+    pyplot.figure(dpi=300)
+    pyplot.axes().set_aspect('equal', 'datalim')
+    pyplot.pcolormesh(layers[:, :, 0])
+    pyplot.show()
